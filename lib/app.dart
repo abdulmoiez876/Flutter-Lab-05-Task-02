a8462d06eff9c16f2378b27390e3c2d14517eb8c
abdulmoiez876 <abdul.moiez876@gmail.com>
2024-03-06 22:26:51 +0500

    task2 step9

--- lib/app.dart
+++ lib/app.dart
@@ -48,27 +48,31 @@ class ShrineApp extends StatelessWidget {
 final ThemeData _kShrineTheme = _buildShrineTheme();
 
 ThemeData _buildShrineTheme() {
-  final ThemeData base = ThemeData.light(useMaterial3: true);
+  final ThemeData base = ThemeData.light();
   return base.copyWith(
     colorScheme: base.colorScheme.copyWith(
-      primary: kShrinePink100,
-      onPrimary: kShrineBrown900,
-      secondary: kShrineBrown900,
+      primary: kShrinePurple,
+      secondary: kShrinePurple,
       error: kShrineErrorRed,
     ),
-    // TODO: Add the text themes (103)
-    // TODO: Decorate the inputs (103)
-    // TODO: Decorate the inputs (103)
+    scaffoldBackgroundColor: kShrineSurfaceWhite,
+    textSelectionTheme: const TextSelectionThemeData(
+      selectionColor: kShrinePurple,
+    ),
+    appBarTheme: const AppBarTheme(
+      foregroundColor: kShrineBrown900,
+      backgroundColor: kShrinePink100,
+    ),
     inputDecorationTheme: const InputDecorationTheme(
       border: CutCornersBorder(),
       focusedBorder: CutCornersBorder(
         borderSide: BorderSide(
           width: 2.0,
-          color: kShrineBrown900,
+          color: kShrinePurple,
         ),
       ),
       floatingLabelStyle: TextStyle(
-        color: kShrineBrown900,
+        color: kShrinePurple,
       ),
     ),
   );
